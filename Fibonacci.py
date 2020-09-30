bc73f3992a7a025acfcdea7e1d4197fe7d50ece4
Kronos-aj <62509931+Kronos-aj@users.noreply.github.com>
2020-10-01 01:22:25 +0530

    Update Fibonacci.py

--- Fibonacci.py
+++ Fibonacci.py
@@ -2,3 +2,16 @@ def fibonacci(n):
     if(n<2):
      return n
     return(fibonacci(n-1)+fibonacci(n-2))
+# 
+def Fibonacci(n):
+    if n<=0:
+        print("Incorrect input")
+    # First Fibonacci number is 0
+    elif n==1:
+        return 0
+    # Second Fibonacci number is 1
+    elif n==2:
+        return 1
+    else:
+        return Fibonacci(n-1)+Fibonacci(n-2)
+    #try this code its better and cares about other expects
